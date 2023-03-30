d862ee75fa583be67b20dfec70cf48a15a22d8ce
TuralJafar <76geemk@code.edu.az>
2023-03-30 22:15:19 +0400

    class araba

---
+++ ConsoleApp12/Araba.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    public class Araba
+    {
+        public int KapiSayi;
+        public string ArabaModeli;
+        public string Arabarengi;
+
+        public Araba()
+        {
+            
+            Console.WriteLine("araba yapici methodu calisti");
+        }
+        public void MotorCalistir()
+        {
+            Console.WriteLine("motor calisiyor");
+        }
+        public void KapilariKilitle()
+        {
+            Console.WriteLine("kapilar kilitlendi");
+        }
+    }
+}
---
+++ ConsoleApp12/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Araba araba1 = new Araba();
+            araba1.KapiSayi = 4;
+            araba1.Arabarengi = "qirmizi";
+            araba1.ArabaModeli = "bmw";
+
+
+            araba1.KapilariKilitle();
+            araba1.MotorCalistir();
+
+            Console.WriteLine("araban kapi sayi=" + araba1.KapiSayi);
+            Console.WriteLine("araba rengi=" + araba1.Arabarengi);
+            Console.WriteLine("araba modeli=" + araba1.ArabaModeli);
+        }
+    }
+}
